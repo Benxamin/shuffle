126f034b133cffdbee264784c0cd51d3ab8a48c7
ben.bertrand <bertrand.design@gmail.com>
2017-01-01 13:05:08 -0700

    Refactors deck into own file. Begins testing Cut.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,37 @@
-var tap = require('tap')
+// Test dependencies
+var tap = require('tap');
+
+// App dependencies
 var shuffle = require('../business_logic/perfect_shuffle.js').shuffle;
-var Deck = shuffle.Deck;
+var Deck = require('../business_logic/deck.js').Deck;
 
-var deck = new Deck();
+// Test the Deck
+tap.test('Deck', function(t) {
+	var deck = new Deck();
+	t.ok(deck.length === 52);
+	t.ok(deck[0] === 'sA');
+	t.ok(deck[12] === 'sK');
+	t.ok(deck[13] === 'hA');
+	t.ok(deck[25] === 'hK');
+	t.ok(deck[26] === 'dA');
+	t.ok(deck[deck.length - 1] === 'cK');
+	t.end();
+});
 
-tap.pass(deck.length === 52);
+// Test the cut
+tap.test('Cut', function(t) {
+	t.ok(shuffle.cut(true) === 'Parameter passed into Cut is not an Array!');
+	t.ok(shuffle.cut(123) === 'Parameter passed into Cut is not an Array!');
+	t.ok(shuffle.cut('foo') === 'Parameter passed into Cut is not an Array!');
+	t.ok(shuffle.cut({}) === 'Parameter passed into Cut is not an Array!');
+	t.ok(shuffle.cut(null) === 'Parameter passed into Cut is not an Array!');
+	t.ok(shuffle.cut(undefined) === 'Parameter passed into Cut is not an Array!');
+
+	// var d = new Deck();
+	// var cutDeck = shuffle.cut(d);
+	// console.log('cutDeck', cutDeck.length);
+
+
+	// t.ok(cutDeck.length === 2);
+	t.end();
+});
